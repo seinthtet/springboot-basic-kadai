deba67a311b7d4ccb731b68e8e905c3e6830671c
Seint Htet <seinthtetsanntint@gmail.com>
2025-05-09 18:42:25 +0900

    spring_kadai_form

---
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -0,0 +1,33 @@
+package com.example.springkadaiform.controller;
+
+import jakarta.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.springkadaiform.form.ContactForm;
+
+@Controller
+public class ContactFormController {
+
+	@GetMapping("/contact")
+    public String showForm(ContactForm contactForm) {
+        return "contactFormView";
+    }
+
+    @PostMapping("/confirm")
+    public String confirmForm(
+            @Valid @ModelAttribute ContactForm contactForm,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "contactFormView";
+        }
+        return "confirmView";
+    }
+}
